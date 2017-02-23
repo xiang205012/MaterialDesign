c7b2cb41a093a5a58899d4ad3e3eee3aa07f8b19
xiang205012 <837022192@qq.com>
2017-02-23 12:14:03 +0800

    first commit

---
+++ app/src/main/java/com/gordon/materialdesign/MainActivity.java
@@ -0,0 +1,19 @@
+package com.gordon.materialdesign;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Warning: Permanently added the RSA host key for IP address '192.30.253.113' to the list of known hosts.
+
+
+
+
+    }
+}
